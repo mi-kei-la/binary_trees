e4105cfe93aac37ad10073ada56dba72e300ae93
mi-kei-la <micaela.percovich@gmail.com>
2021-03-10 17:02:20 +0000

    update tasks

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,11 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_is_perfect
+  * binary_tree_is_perfect - Check if a binary tree is perfect.
+  *
+  * @tree: Tree to check.
+  *
+  * Return: 1 if perfect, 0 otherwise.
   */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
